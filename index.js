760738d95ebbda62cc343e56b9dd9d8f2a32f7db
Chris Parnin <chris.parnin@gmail.com>
2019-03-30 10:00:34 -0400

    Update command name.

--- index.js
+++ index.js
@@ -4,13 +4,10 @@ const chalk = require('chalk');
 
 const Micro = require('./lib/micro');
 
-// Register run command
-yargs.command('build', 'build', (yargs) => { }, async (argv) => {
-
-    console.log('zzzß')
+// Build
+yargs.command('run', 'Provision a new micro kernel', (yargs) => { }, async (argv) => {
     let micro = new Micro();
     await micro.create("micro",{}).catch( e => console.log(e));
-
 });
 
 // Turn on help and access argv
