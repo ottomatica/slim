b34d404aa23998139244b31deb739d06d2232795
Chris Parnin <chris.parnin@gmail.com>
2019-03-30 20:55:09 -0400

    Documentation; setup ssh key

---
+++ README.md
@@ -0,0 +1,34 @@
+# slim
+
+`slim` will build a microkernel from a Dockerfile. Slim works by building and extracting a rootfs from a Dockerfile, and then merging that filesystem with a small minimal kernel that runs in RAM.
+
+This results in a real VM that can boot instantly, while using very limited resources.
+
+## Using slim
+
+`slim build images/alpine3.8-runc-ansible`: Create a microkernel from a Dockerfile. [See example Dockerfile](https://github.com/ottomatica/slim/tree/master/images/alpine3.8-runc-ansible). This will be stored as a bootable iso.
+
+`slim images`: See a list of microkernels on your machine.
+
+![images command](doc/img/images.png)
+
+`slim run micro1 alpine3.8-runc-ansible`: Provision a new instance of the given microkernel as a VM in VirtualBox. VirtualBox will run the microkernel as an attached iso loaded into a cdrom, and boot up the iso in seconds.
+
+For convience, a ssh connection command is provided at the end of the command, allowing easy access into the machine:
+Example: `ssh -i /Users/cjparnin/projects/slim/scripts/keys/baker_rsa root@127.0.0.1 -p 2008 -o StrictHostKeyChecking=no`
+
+## Installing slim
+
+Simply clone this repo and run.
+
+```
+npm install
+npm link
+```
+
+Unfortunately, due to the experimental nature, there are a few dependencies you must also install:
+
+* [VirtualBox](https://www.virtualbox.org/wiki/Downloads)
+* [docker](https://docs.docker.com/install/)
+* gtar: `brew install gnu-tar`
+* cdrtools: `brew install cdrtools`
--- index.js
+++ index.js
@@ -14,55 +14,66 @@ const Images= require('./lib/images');
 // - prereqs
 // - permissions
 // - required files
-const {registery,env}  = new Env().setup().check().vars();
-
-// Create VM
-yargs.command('run <name> <image>', 'Provision a new micro kernel', (yargs) => { }, async (argv) => {
-    let micro = new Micro();
-    await micro.create(argv.name, registery, {attach_iso:argv.image}).catch( e => console.log(e));
-});
-
-// Images
-yargs.command('images', 'List available images', (yargs) => { }, async (argv) => {
-
-    let images = new Images();
-    let table = await images.list(registery);
-    let transformed = table.reduce((table, {image, ...x}) => { table[image] = x; return table}, {})
-
-    console.table(transformed);
-});
-
-yargs.command('build [path]', 'Build a new micro kernel', (yargs) => { }, async (argv) => {
-
-    let buildPath = argv.path || path.join(__dirname,'images/alpine3.8-runc-ansible');
-    buildPath = path.resolve(buildPath);
-
-    let name = path.basename(buildPath);
-    let outputPath = path.join(registery, name, 'slim.iso');
-    let baseIso = path.join(registery, name, 'base.iso');
-    let infoPath = path.join(buildPath, 'info.yml');
-
-    if( !fs.existsSync(buildPath)) { console.log(`path does not exist: ${buildPath}`); return; }
-    if( !fs.existsSync(path.join(buildPath, 'Dockerfile'))) { console.log(`Expected Dockerfile does not in this path: ${buildPath}`); return; }
-    if( !fs.existsSync(infoPath)) { console.log(`Expected required configuration file missing: ${infoPath}`); return; }
-
-    // Fetch required base images
-    let info = await yaml.safeLoad(fs.readFileSync(infoPath));
-    let iso = info.base_url;
-    await env.fetch(iso, path.dirname(outputPath), 'base.iso');
-
-    if( !fs.existsSync( path.dirname(outputPath)) )
-    {
-        fs.mkdirSync(path.dirname(outputPath));
-    }
-    child.execSync(`scripts/extract-fs.sh ${buildPath}`, {stdio: 'inherit'});
-    child.execSync(`scripts/make-iso.sh ${outputPath} ${baseIso}`, {stdio: 'inherit'})
-
-    // Copy over to output directory
-    fs.copyFileSync(infoPath, path.join(path.dirname(outputPath),'info.yml'));
-
-});
 
 
-// Turn on help and access argv
-yargs.help().argv;
+(async () => {
+
+    let env = new Env();
+    await env.setup();
+    let {registery} = env.check().vars();
+
+    // Create VM
+    yargs.command('run <name> <image>', 'Provision a new micro kernel', (yargs) => { }, async (argv) => {
+        let micro = new Micro();
+        await micro.create(argv.name, registery, {attach_iso:argv.image}).catch( e => console.log(e));
+    });
+
+    // Images
+    yargs.command('images', 'List available images', (yargs) => { }, async (argv) => {
+
+        let images = new Images();
+        let table = await images.list(registery);
+        let transformed = table.reduce((table, {image, ...x}) => { table[image] = x; return table}, {})
+
+        console.table(transformed);
+    });
+
+    yargs.command('build [path]', 'Build a new micro kernel', (yargs) => { }, async (argv) => {
+
+        let buildPath = argv.path || path.join(__dirname,'images/alpine3.8-runc-ansible');
+        buildPath = path.resolve(buildPath);
+
+        let name = path.basename(buildPath);
+        let outputPath = path.join(registery, name, 'slim.iso');
+        let baseIso = path.join(registery, name, 'base.iso');
+        let infoPath = path.join(buildPath, 'info.yml');
+
+        if( !fs.existsSync(buildPath)) { console.log(`path does not exist: ${buildPath}`); return; }
+        if( !fs.existsSync(path.join(buildPath, 'Dockerfile'))) { console.log(`Expected Dockerfile does not in this path: ${buildPath}`); return; }
+        if( !fs.existsSync(infoPath)) { console.log(`Expected required configuration file missing: ${infoPath}`); return; }
+
+        // Fetch required base images
+        let info = await yaml.safeLoad(fs.readFileSync(infoPath));
+        let iso = info.base_url;
+        await env.fetch(iso, path.dirname(outputPath), 'base.iso');
+
+        if( !fs.existsSync( path.dirname(outputPath)) )
+        {
+            fs.mkdirSync(path.dirname(outputPath));
+        }
+        child.execSync(`scripts/extract-fs.sh ${buildPath}`, {stdio: 'inherit'});
+        child.execSync(`scripts/make-iso.sh ${outputPath} ${baseIso}`, {stdio: 'inherit'})
+
+        // Copy over to output directory
+        fs.copyFileSync(infoPath, path.join(path.dirname(outputPath),'info.yml'));
+
+    });
+
+
+    // Turn on help and access argv
+    yargs.help().argv;
+
+})();
+
+
+
