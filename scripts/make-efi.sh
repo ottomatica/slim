f4d54509adeb05947bcfb22ca92f4b631e4a15af
Chris Parnin <chris.parnin@gmail.com>
2022-01-02 15:03:33 -0500

    fixes

--- scripts/make-efi.sh
+++ scripts/make-efi.sh
@@ -2,41 +2,15 @@
 echo "Creating disk sized $1M"
 dd if=/dev/zero of=rootfs.ext4 bs=1M count=$1
 
-# to create the partitions programatically (rather than manually)
-# we're going to simulate the manual input to fdisk
-# The sed script strips off all the comments so that we can 
-# document what we're doing in-line with the actual commands
-# Note that a blank line (commented as "defualt" will send a empty
-# line terminated with a newline to take the fdisk default.
-# sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk rootfs.ext4
-#   o # clear the in memory partition table
-#   n # new partition
-#   p # primary partition
-#   1 # partition number 1
-#     # default - start at beginning of disk 
-#   +100M # 100 MB boot parttion
-#   t # Change partition type
-#   ef # EFI (FAT-12/16/32)
-#   n # new partition
-#   p # primary partition
-#   2 # partion number 2
-#     # default, start immediately after preceding partition
-#     # default, extend partition to end of disk
-#   a # make a partition bootable
-#   1 # bootable partition is partition 1 -- /dev/sda1
-#   p # print the in-memory partition table
-#   w # write the partition table
-#   q # and we're done
-# EOF
-
+# Create two partitions (ESP for boot and second for rootfs).
 sgdisk --clear \
   --new 1::+100M --typecode=1:ef00 --change-name=1:'EFI System' \
   --new 2::-0 --typecode=2:8300 --change-name=2:'slim-rootfs' \
   --attributes 1:set:2 \
   rootfs.ext4
-
   # --new 1::+1M --typecode=1:ef02 --change-name=1:'BIOS boot partition' \
 
+# Print partitions
 gdisk -l rootfs.ext4
 
 # Automatically mount partitions on loop devices.
@@ -66,7 +40,7 @@ done
 
 lsblk
 
-# Format ESI partition (ESP) and install bootloader (syslinux)
+# Format ESI partition (ESP)
 mkfs.fat ${LOOPDEV}p1
 ESP=/tmp/esp
 mkdir $ESP && mount ${LOOPDEV}p1 $ESP
@@ -83,6 +57,7 @@ mkdir -p $ESP/EFI/BOOT
 #echo "INITRD initrd" >> $ESP/EFI/BOOT/syslinux.cfg
 #echo "APPEND root=/dev/sda2 console=tty0 console=ttyS0,115200n8" >> $ESP/EFI/BOOT/syslinux.cfg
 
+# GRUB 
 cat >> $ESP/EFI/BOOT/grub.cfg <<EOF
 set timeout=0
 set gfxpayload=text
@@ -94,16 +69,9 @@ EOF
 
 GRUB_MODULES="part_gpt part_msdos efi_uga gptsync fat ext2 lvm iso9660 lsefi gzio linux linuxefi acpi normal cpio crypto disk boot crc64 \
 search_fs_uuid tftp xzio lzopio xfs video scsi multiboot hfsplus udf"
-# chroot /tmp/rootfs /bin/bash
-#apt-get update
-#apt-get -y install grub-efi
-#grub-install --target=x86_64-efi --efi-directory=$ESP/EFI/BOOT --boot-directory=$ESP/EFI/BOOT --bootloader-id=grub ${ESP} 
+
 grub-mkimage -d /usr/lib/grub/x86_64-efi -O x86_64-efi -o BOOTX64.EFI -p /EFI/BOOT ${GRUB_MODULES} linuxefi;
 cp BOOTX64.EFI $ESP/EFI/BOOT/BOOTX64.EFI
-
-# Syslinux needs kernel and initrd on same partition.
-cp /slim-vm/boot/vmlinuz $ESP/EFI/BOOT/vmlinuz
-cp /slim-vm/boot/initrd $ESP/EFI/BOOT/initrd
 
 # Prepare rootfs partition
 mkfs.ext4 -F -L "slim-rootfs" ${LOOPDEV}p2
@@ -115,36 +83,22 @@ mkdir -p /tmp/rootfs/boot/grub
 echo "Copying rootfs"
 
 #cp -a /slim-vm/. /tmp/rootfs
-rsync -a /slim-vm/ /tmp/rootfs
+#rsync -a /slim-vm/ /tmp/rootfs
+tar -xf /slim-vm/rootfs.tar -C /tmp/rootfs
 
 echo "LABEL=slim-rootfs	/	 ext4	discard,errors=remount-ro	0 1" >> /tmp/rootfs/etc/fstab
 
-# Label
-#tune2fs -O ^read-only -L "slim-rootfs" ${LOOPDEV}p2
-
-# Grub stuff
-# cat > /tmp/rootfs/boot/grub/device.map << EOF
-# (hd0)   ${LOOPDEV}
-# (hd0,msdos1) ${LOOPDEV}p1
-# EOF
-
-
-# grub-install   --no-floppy \
-#               --grub-mkdevicemap=/tmp/rootfs/boot/grub/device.map \
-#               --modules="part_msdos ext2 configfile normal search_fs_uuid" \
-#               --target=x86_64-efi \
-#               --efi-directory=${ESP}/EFI/BOOT \
-#               --root-directory=/tmp/rootfs \
-#               --debug \
-#               ${LOOPDEV}
+# Syslinux/Grub needs kernel and initrd on same partition.
+mv /tmp/rootfs/vmlinuz $ESP/EFI/BOOT/vmlinuz
+mv /tmp/rootfs/initrd $ESP/EFI/BOOT/initrd
 
 # Cleanup
 umount ${LOOPDEV}p1
 umount ${LOOPDEV}p2
 losetup -d ${LOOPDEV}
 
+# Sanity check disk
 fsck -f rootfs.ext4
-
 
 # Store back on host
 mv rootfs.ext4 /out/rootfs
