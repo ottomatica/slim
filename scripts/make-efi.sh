dcac666be870c1889773db6dcd2bf8d6823d5319
Chris Parnin <chris.parnin@gmail.com>
2022-01-02 12:52:02 -0500

    attempt #99

--- scripts/make-efi.sh
+++ scripts/make-efi.sh
@@ -8,26 +8,34 @@ dd if=/dev/zero of=rootfs.ext4 bs=1M count=$1
 # document what we're doing in-line with the actual commands
 # Note that a blank line (commented as "defualt" will send a empty
 # line terminated with a newline to take the fdisk default.
-sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk rootfs.ext4
-  o # clear the in memory partition table
-  n # new partition
-  p # primary partition
-  1 # partition number 1
-    # default - start at beginning of disk 
-  +100M # 100 MB boot parttion
-  t # Change partition type
-  ef # EFI (FAT-12/16/32)
-  n # new partition
-  p # primary partition
-  2 # partion number 2
-    # default, start immediately after preceding partition
-    # default, extend partition to end of disk
-  a # make a partition bootable
-  1 # bootable partition is partition 1 -- /dev/sda1
-  p # print the in-memory partition table
-  w # write the partition table
-  q # and we're done
-EOF
+# sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk rootfs.ext4
+#   o # clear the in memory partition table
+#   n # new partition
+#   p # primary partition
+#   1 # partition number 1
+#     # default - start at beginning of disk 
+#   +100M # 100 MB boot parttion
+#   t # Change partition type
+#   ef # EFI (FAT-12/16/32)
+#   n # new partition
+#   p # primary partition
+#   2 # partion number 2
+#     # default, start immediately after preceding partition
+#     # default, extend partition to end of disk
+#   a # make a partition bootable
+#   1 # bootable partition is partition 1 -- /dev/sda1
+#   p # print the in-memory partition table
+#   w # write the partition table
+#   q # and we're done
+# EOF
+
+sgdisk --clear \
+  --new 1::+100M --typecode=1:ef00 --change-name=1:'EFI System' \
+  --new 2::-0 --typecode=2:8300 --change-name=2:'slim-rootfs' \
+  --attributes 1:set:2 \
+  rootfs.ext4
+
+  # --new 1::+1M --typecode=1:ef02 --change-name=1:'BIOS boot partition' \
 
 # Automatically mount partitions on loop devices.
 # losetup --partscan --find --show rootfs.ext4
@@ -57,7 +65,7 @@ done
 lsblk
 
 # Format ESI partition (ESP) and install bootloader (syslinux)
-mkdosfs ${LOOPDEV}p1
+mkfs.fat ${LOOPDEV}p1
 ESP=/tmp/esp
 mkdir $ESP && mount ${LOOPDEV}p1 $ESP
 mkdir -p $ESP/EFI/BOOT
@@ -88,7 +96,7 @@ search_fs_uuid tftp xzio lzopio xfs video scsi multiboot"
 #apt-get update
 #apt-get -y install grub-efi
 #grub-install --target=x86_64-efi --efi-directory=$ESP/EFI/BOOT --boot-directory=$ESP/EFI/BOOT --bootloader-id=grub ${ESP} 
-grub-mkimage -O x86_64-efi -o BOOTX64.EFI -p /EFI/BOOT ${GRUB_MODULES} linuxefi;
+grub-mkimage -d /usr/lib/grub/x86_64-efi -O x86_64-efi -o BOOTX64.EFI -p /EFI/BOOT ${GRUB_MODULES} linuxefi;
 cp BOOTX64.EFI $ESP/EFI/BOOT/BOOTX64.EFI
 
 # Syslinux needs kernel and initrd on same partition.
@@ -96,18 +104,20 @@ cp /slim-vm/boot/vmlinuz $ESP/EFI/BOOT/vmlinuz
 cp /slim-vm/boot/initrd $ESP/EFI/BOOT/initrd
 
 # Prepare rootfs partition
-mkfs.ext2 ${LOOPDEV}p2
+mkfs.ext4 ${LOOPDEV}p2
 mkdir -p /tmp/rootfs 
-mount -t ext2 ${LOOPDEV}p2 /tmp/rootfs
+mount -t ext4 ${LOOPDEV}p2 /tmp/rootfs
 mkdir -p /tmp/rootfs/boot/grub
 
 # Copy extracted rootfs into mounted image
 echo "Copying rootfs"
-cp -a /slim-vm/. /tmp/rootfs
+#cp -a /slim-vm/. /tmp/rootfs
+rsync -a /slim-vm/ /tmp/rootfs/
+
 echo "LABEL=slim-rootfs	/	 ext4	discard,errors=remount-ro	0 1" >> /tmp/rootfs/etc/fstab
 
 # Label
-tune2fs -O ^read-only -L "slim-rootfs" ${LOOPDEV}p2
+#tune2fs -O ^read-only -L "slim-rootfs" ${LOOPDEV}p2
 
 # Grub stuff
 # cat > /tmp/rootfs/boot/grub/device.map << EOF
